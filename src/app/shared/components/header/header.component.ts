4a2b06db27e1fbb80e5ac5b4b2e88e91852635fc
Rhamanda Lima <cathyannarhamanda@gmail.com>
2023-11-21 19:13:15 -0300

    feat: create header layout

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  public hour = '';
 
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.loadTime();
+    setInterval(() => { this.loadTime() }, 5000);
+  }
+
+  public navigateToHome(): void {
+    this.router.navigate(['/']);
+  }
+
+  private loadTime(): void {
+    this.hour = new Date().toLocaleTimeString('pt-BR').slice(0, 5);
+  }
 }
